f760436da9893370ad9881091d5ec929d43ca77c
Vimal V Nair <vimalvnair999@gmail.com>
2021-06-25 20:22:08 +0530

    Add min_age filter - Get centers for 2 weeks

--- poll_vaccine_telegram.rb
+++ poll_vaccine_telegram.rb
@@ -7,10 +7,15 @@ TELEGRAM_BOT_API_KEY = ENV['TELEGRAM_BOT_API_KEY']
 TELEGRAM_CHAT_ID = ENV['TELEGRAM_CHAT_ID']
 TELEGRAM_ERROR_CHANNEL_CHAT_ID = ENV['TELEGRAM_ERROR_CHANNEL_CHAT_ID']
 DISTRICT_ID = ENV['DISTRICT_ID']
+MIN_AGE_LIMIT = ENV['MIN_AGE_LIMIT']
+
+def filter_condition session
+  session['available_capacity'] > 0 && (MIN_AGE_LIMIT.nil? || session['min_age_limit'].to_i == MIN_AGE_LIMIT.to_i)
+end
 
 def get_session_details centers
   sessions = centers.map{|c| c['sessions'].map{|s| s.merge(c.select{|k,v| k != "sessions"} || {} )}}.flatten
-  available_sessions =  sessions.select{|s| s['available_capacity'] > 0}
+  available_sessions =  sessions.select{|session| filter_condition session }
   return ["no sessions"] if available_sessions.nil? || available_sessions.empty?
   puts available_sessions.count
   details = available_sessions.map{|s|
@@ -22,12 +27,12 @@ Dose 1 Capacity: <b>#{s['available_capacity_dose1']}</b>
 Dose 2 Capacity: <b>#{s['available_capacity_dose2']}</b>
 Total Capacity: <b>#{s['available_capacity']}</b>
 <i>-------------------------</i>
-)                              
+)
   }
 end
 
 def send_telegram_message message, chat_id, parse_mode = 'html'
-  puts message
+  #puts message
   uri = URI("https://api.telegram.org/bot#{TELEGRAM_BOT_API_KEY}/sendMessage?chat_id=#{chat_id}&parse_mode=#{parse_mode}&text=#{CGI.escape(message)}")
   req = Net::HTTP::Get.new(uri)
   res = Net::HTTP.start(uri.hostname, uri.port, use_ssl: true) do |http|
@@ -39,14 +44,11 @@ def send_telegram_message message, chat_id, parse_mode = 'html'
   puts res.body
 end
 
-current_key = ""
-previous_key = ""
-
-loop do
+def fetch_centers date
   begin
-    today = Date.today.strftime("%d-%m-%Y")
-    uri = URI("https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id=#{DISTRICT_ID}&date=#{today}")
-    puts "Date: #{today}"
+    uri = URI("https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id=#{DISTRICT_ID}&date=#{date}")
+    puts "Date: #{date}"
+    puts uri.to_s
     req = Net::HTTP::Get.new(uri)
     req['User-Agent'] = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.128 Safari/537.36"
 
@@ -56,27 +58,54 @@ loop do
     }
     puts "http_code: #{resp.code}"
 
+    centers = []
     if resp.code == "200"
       json = JSON.parse(resp.body)
+      puts json
       centers = json['centers']
-      sessions = centers.map{|c| c['sessions']}.flatten
-      current_key = sessions.select{|s| s['available_capacity'] > 0}.map{|s| "#{s['session_id']}#{s['available_capacity']}"}.sort.join
-
-      puts current_key
-
-      if current_key != previous_key
-        session_details = get_session_details centers
-        session_details.each_slice(17).each do |session_slice|
-          send_telegram_message session_slice.join("\n"), TELEGRAM_CHAT_ID
-          sleep 2
-        end
-      end
-
-      previous_key = current_key
     else
       send_telegram_message("Error, response code: #{resp.code}", TELEGRAM_ERROR_CHANNEL_CHAT_ID, "")
     end
+    puts centers.inspect
+    centers
   rescue Exception => e
+    send_telegram_message e.message, TELEGRAM_ERROR_CHANNEL_CHAT_ID, "" rescue nil
+  ensure
+    centers
+  end
+end
+
+current_key = ""
+previous_key = ""
+
+loop do
+  begin
+    dates = [Date.today, Date.today + 7]
+
+    centers = []
+    dates.each do |date|
+      centers << fetch_centers(date.strftime("%d-%m-%Y"))
+    end
+    centers = centers.flatten
+
+    sessions = centers.map{|c| c['sessions']}.flatten
+    current_key = sessions
+                    .select{|session| filter_condition session}
+                    .map{|s| "#{s['session_id']}#{s['available_capacity']}"}.sort.join
+
+    puts current_key
+
+    if current_key != previous_key
+      session_details = get_session_details centers
+      session_details.each_slice(17).each do |session_slice|
+        send_telegram_message session_slice.join("\n"), TELEGRAM_CHAT_ID
+        sleep 2
+      end
+    end
+
+    previous_key = current_key
+  rescue Exception => e
+    puts e.inspect
     send_telegram_message e.message, TELEGRAM_ERROR_CHANNEL_CHAT_ID, "" rescue nil
   ensure
     sleep 25
